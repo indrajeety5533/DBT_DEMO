c91e83be408fd1719410694e7b3eb6fc768b4f23
Indrajeet Yadav <indrajeety5533@gmail.com>
2026-09-08 04:10:12 +0000

    final_runing_modelswith_data_2018

--- macros/my_macro.sql
+++ macros/my_macro.sql
@@ -1,20 +1,22 @@
 {% macro get_session(x) %}
 
     case
-        when month(TO_TIMESTAMP({{x}})) in (12,1,2) then 'winter'
-        when month(TO_TIMESTAMP({{x}})) in (3,4,5) then 'spring'
-        when month(TO_TIMESTAMP({{x}})) in (12,1,2) then 'summer' else 'autumn' 
+        when month(to_timestamp({{ x }})) in (12, 1, 2) then 'winter'
+        when month(to_timestamp({{ x }})) in (3, 4, 5) then 'spring'
+        when month(to_timestamp({{ x }})) in (6, 7, 8) then 'summer'
+        else 'autumn'
     end
 
 {% endmacro %}
 
 
+
 {% macro Day_type(x) %}
 
-case
-    when DAYNAME(TO_TIMESTAMP({{ x }})) in ('sat', 'sun')
-        then 'Weekend'
-    else 'weekdays'
-end
+    case
+        when dayname(to_timestamp({{ x }})) in ('Sat', 'Sun')
+            then 'Weekend'
+        else 'Weekday'
+    end
 
 {% endmacro %}
