9b372513dbef231b6db7c266b2bbff1099663aca
Indrajeet Yadav <indrajeety5533@gmail.com>
2026-09-08 03:18:50 +0000

    Date_Dimension model created

--- models/Date_Dimension.sql
+++ models/Date_Dimension.sql
@@ -1,19 +1,17 @@
 WITH cte AS (
 
-    SELECT 
-    
-    TO_TIMESTAMP(STARTED_AT) AS STARTED_AT,
-    DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
-    HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
+    SELECT*
+        --TO_TIMESTAMP(STARTED_AT) AS STARTED_AT,
+        --DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
+        --HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
 
-    {{Day_type('STARTED_AT')}} as day_type,
+        {{ Day_type('STARTED_AT') }} AS day_type,
 
-    {{get_session('STARTED_AT')}} AS station_of_year
+        {{ get_session('STARTED_AT') }} AS station_of_year
 
-
-
-     FROM {{ source('demo', 'bike') }}
+    FROM {{ ref('stg_bike') }}
 
 )
 
-SELECT * FROM cte
+SELECT *
+FROM cte;
